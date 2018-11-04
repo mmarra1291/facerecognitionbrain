41a6eeb7af1825626dd77e0af027a01a7cc983a6
Matt Marrandino <marrandino1291@yahoo.com>
2018-11-04 09:13:46 -0600

    Today's Updates

--- App.js
+++ App.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
-import Navigation from './components/Navigation/Navigation'
-import Logo from './components/Logo/Logo'
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <Navigation />
+        <Navigation />
         <Logo />
-     {/* 
-       <ImageLinkForm />
-       <FaceRecognition />*/}
+        <ImageLinkForm />
+       {
+       /*<FaceRecognition />*/}
       </div>
     );
   }
