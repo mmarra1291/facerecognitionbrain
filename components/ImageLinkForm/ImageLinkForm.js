b77988de509d2f9ebfaeef66bf19ce81f5d37168
Matt Marrandino <marrandino1291@yahoo.com>
2018-11-05 07:01:16 -0600

    ImageLinkForm Styling Updates

--- components/ImageLinkForm/ImageLinkForm.js
+++ components/ImageLinkForm/ImageLinkForm.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './ImageLinkForm.css';
 
 const ImageLinkForm = () => {
 	return (
@@ -7,7 +8,7 @@ const ImageLinkForm = () => {
 		{'This Magic Brain Will Detect Faces in Your Pictures'}
 		</p>
 		<div className='center'>
-			<div className='pa4 br3 shadow-5'>
+			<div className='form pa4 br3 shadow-5'>
 				<input className='f4 pa2 w-70 center' type='tex' />
 				<button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
 			</div>
