2b949a2768bf7cd050f56ecb7c74077abae27614
Matt Marrandino <marrandino1291@yahoo.com>
2018-11-06 06:59:03 -0600

    Today's updates

--- components/ImageLinkForm/ImageLinkForm.js
+++ components/ImageLinkForm/ImageLinkForm.js
@@ -8,7 +8,7 @@ const ImageLinkForm = () => {
 		{'This Magic Brain Will Detect Faces in Your Pictures'}
 		</p>
 		<div className='center'>
-			<div className='form pa4 br3 shadow-5'>
+			<div className='form center pa4 br3 shadow-5'>
 				<input className='f4 pa2 w-70 center' type='tex' />
 				<button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
 			</div>
